22e2dbec74d6947cf3766a35c6d9b804ef332a00
aaroncu <aaroncu@hotmail.com>
2017-05-18 01:10:41 -0400

    added edit functionality, clear tasks functionality and updating tasks functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,60 @@
 $(document).ready(function(){
+  //Add Task Event
   $('#add-task-form').on('submit', function(e){
     addTask(e);
   });
+  
+  //Edit Event
+  $('#edit-task-form').on('submit', function(e){
+    updateTask(e);
+  });
+  
+  //Remove task event
+  $('#task-table').on('click','#remove-task', function(){
+    id = $(this).data('id');
+    removeTask(id);
+  });
+  
+  //Clear tasks event
+  $('#clear-tasks').on('click', function(){
+    clearAllTasks();
+  });
+  
+  displayTasks();
+  
+  //Function to display tasks
+  function displayTasks(){
+    var taskList = JSON.parse(localStorage.getItem('tasks'));
+    
+    //Sort Tasks
+    if(taskList != null){
+      taskList = taskList.sort(sortByTime);
+    }
+    
+    /// Set Counter
+    var i = 0;
+    
+    //Check Tasks
+    if(localStorage.getItem('tasks') != null){
+      //Loop Through and Display
+      $.each(taskList, function(key, value){
+        $('#task-table').append('<tr id="'+ value.id +'">' + 
+                               '<td>' + value.task + '</td>' +
+                               '<td>' + value.task_priority + '</td>' +
+                               '<td>' + value.task_date + '</td>' +
+                               '<td>' + value.task_time + '</td>' +
+                               '<td><a href="edit.html?id='+ value.id +'">Edit</a> | <a href="#" id="remove-task" data-id="'+ value.id + '">Remove</a></td>' +
+                               '</tr>');
+      })
+    }  
+  }
+  
+  //Function to sort taks
+  function sortByTime(a, b){
+    var aTime = a.task_time;
+    var bTime = b.task_time;
+    return ((aTime < bTime) ? -1 : ((aTime > bTime) ? 1 : 0));
+  }
   
   
   //Function to add a task
@@ -57,4 +110,118 @@ $(document).ready(function(){
     }
   }
   
+  //Function to update tasks
+  function updateTask(e){
+    var id = $('#task_id').val();
+    var task = $('#task').val();
+    var task_priority = $('#priority').val();
+    var task_date = $('#date').val();
+    var task_time = $('#time').val();
+    
+    taskList = JSON.parse(localStorage.getItem('tasks'));
+    
+    for(var i=0; i < taskList.length; i++){
+      if(taskList[i].id == id){
+        taskList.splice(i,1)
+      }
+      localStorage.setItem('tasks', JSON.stringify(taskList));
+    }
+    
+    
+    if(task == ''){
+      alert('Task is required');
+      e.preventDefault();
+    }
+    else if(task_date ==''){
+      alert('Date is required');
+      e.preventDefault();
+    }
+    else if(task_time ==''){
+      alert('Time is required');
+      e.preventDefault();
+    }
+    else if(task_priority ==''){
+      task_priority = 'normal';
+    } 
+    else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    
+      //Check Tasks
+      if(tasks == null){
+        tasks = [];
+      }
+      
+      var taskList = JSON.parse(localStorage.getItem('tasks'));
+      
+      //New Task Object
+      var new_task = {
+        "id": id,
+        "task": task,
+        "task_priority": task_priority,
+        "task_date": task_date,
+        "task_time": task_time
+      }
+      
+      tasks.push(new_task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      
+    }
+  }
+  
+  //Function to remove tasks
+  function removeTask(id){
+    if(confirm('are you sure you want to delete this task?')){
+      var taskList = JSON.parse(localStorage.getItem('tasks'));
+       for(var i=0; i < taskList.length; i++){
+      if(taskList[i].id == id){
+        taskList.splice(i,1)
+      }
+      localStorage.setItem('tasks', JSON.stringify(taskList));
+    }
+      
+      location.reload();
+    }
+  }
+  
+  //Function to clear all tasks
+  function clearAllTasks(){
+    if(confirm('Do you want to clear all tasks?')){
+      localStorage.clear();
+      location.reload();
+    }     
+  }
+  
 });
+
+//Function for getting single tasks
+function getTask(){
+  var $_GET = getQueryParams(document.location.search);
+  id = $_GET['id'];
+  
+  var taskList = JSON.parse(localStorage.getItem('tasks'));
+  
+  for(var i=0; i < taskList.length; i++){
+    if(taskList[i].id == id){
+      $('#edit-task-form #task_id').val(taskList[i].id);
+      $('#edit-task-form #task').val(taskList[i].task);
+      $('#edit-task-form #priority').val(taskList[i].task_priority);
+      $('#edit-task-form #date').val(taskList[i].task_date);
+      $('#edit-task-form #time').val(taskList[i].task_time);
+    }
+  }
+}
+
+//Function to get HTTP Get Requests
+function getQueryParams(qs) {
+  qs = qs.split("+").join(" ");
+  var params = {},
+      tokens,
+      re = /[?&]?([^=]+)=([^&]*)/g;
+  
+  while (tokens = re.exec(qs)) {
+    params[decodeURIComponent(tokens[1])]
+        = decodeURIComponent(tokens[2]);
+  }
+  
+  return params;
+}
